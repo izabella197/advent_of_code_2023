2fcf87b0b7267c240cc06fd0f19b6168f14d0891
julia.grabos <julia@tines.io>
2023-12-11 18:45:20 +0100

    first commit

---
+++ 1/advent_of_code_1.py
@@ -0,0 +1,63 @@
+import re
+
+def test():
+    input = open("input.txt", "r")
+
+    processed = []
+    for idx, item in enumerate(input):
+        extracted_number = re.findall(r'\d', item)
+        
+        processed.append("%s%s" % (extracted_number[0], extracted_number[-1]))
+
+
+    total = 0
+    
+    for item in processed:
+        total += int(item)
+
+    print(total)
+
+def test2():
+    input = open("input.txt", "r")
+    processed = []
+
+
+    mapping = {"one": 1,
+               "two": 2,
+               "three": 3,
+               "four": 4,
+               "five": 5,
+               "six": 6,
+               "seven": 7,
+                "eight": 8,
+               "nine":9}
+        
+                    
+     
+    for idx, item in enumerate(input):
+        extracted_number = re.findall(r'(?=(\d|one|two|three|four|five|six|seven|eight|nine))', item)
+
+
+        to_append = "%s%s" % (mapping.get(extracted_number[0], extracted_number[0]), mapping.get(extracted_number[-1],extracted_number[-1]))
+
+      
+        print(item, extracted_number ,to_append)
+
+        #print(item, extracted_number, to_append)
+        processed.append(to_append)
+
+
+
+    t = 0
+    
+    for item in processed:
+        t += int(item)
+
+    print(t)
+    
+        
+    
+if __name__ == "__main__":
+    # test()
+    test2()
+    
